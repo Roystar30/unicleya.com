93aa7b6b895c78432197ebb6aa6d8bc60c7c5a24
Roystar30 <92625853+Roystar30@users.noreply.github.com>
2024-05-03 12:55:01 +0530

    Update script.js
    blank
    
    
    Signed-off-by: Roystar30 <92625853+Roystar30@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-function toggleNav() {
-    var navIcon = document.getElementById("navIcon");
-    var closeBtn = document.querySelector(".closebtn");
-    var sideNav = document.getElementById("mySidenav");
 
-    if (navIcon.classList.contains("active")) {
-        // Close navigation
-        navIcon.classList.remove("active");
-        closeBtn.style.display = "none";
-        sideNav.style.left = "-250px";
-    } else {
-        // Open navigation
-        navIcon.classList.add("active");
-        closeBtn.style.display = "block";
-        sideNav.style.left = "0";
-    }
-}
