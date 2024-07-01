902c17cb77db9435f5840d212531d17e92eb3572
Roystar30 <92625853+Roystar30@users.noreply.github.com>
2024-07-01 23:19:47 +0530

    Update script.js
    connected to new index for nave bar action
    
    Signed-off-by: Roystar30 <92625853+Roystar30@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.querySelector('.openbtn').classList.add('transform');
+}
 
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.querySelector('.openbtn').classList.remove('transform');
+}
